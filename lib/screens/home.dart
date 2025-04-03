9170bc2e4ecb226f1588284400cd7aeace84dd2e
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2025-04-03 20:17:45 -0300

    Config inicial de Routes and Image Splash

---
+++ lib/screens/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../routes/app.routes.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Página Inicial')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, AppRoutes.profile);
+              },
+              child: const Text('Ir para Perfil'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, AppRoutes.settings);
+              },
+              child: const Text('Ir para Configurações'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
