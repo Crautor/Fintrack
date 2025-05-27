b48a8279812ac479fde4f0d04d252bb685c84bfd
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2025-05-26 21:26:17 -0300

    fix: navbar

--- lib/components/layout/CategoriesTab/categories_tab.dart
+++ lib/components/layout/CategoriesTab/categories_tab.dart
@@ -1,6 +1,7 @@
-import 'package:fintrack/screens/Categories/categories.dart';
-import 'package:fintrack/screens/Categories/expense_add.dart';
 import 'package:flutter/material.dart';
+import 'package:fintrack/screens/Categories/categories.dart';
+import 'package:fintrack/screens/Categories/transictions_add.dart';
+import 'package:fintrack/screens/Categories/category_detail.dart';
 
 class CategoriesTab extends StatefulWidget {
   const CategoriesTab({super.key});
@@ -10,15 +11,47 @@ class CategoriesTab extends StatefulWidget {
 }
 
 class _CategoriesTabState extends State<CategoriesTab> {
-  bool showAddExpense = false;
+  String? currentView;
+  Map<String, dynamic>? selectedCategory;
 
-  void openAddExpense() => setState(() => showAddExpense = true);
-  void backToCategories() => setState(() => showAddExpense = false);
+  void openAddTransictions() {
+    setState(() {
+      currentView = 'add';
+    });
+  }
+
+  void openCategoryDetail(Map<String, dynamic> category) {
+    setState(() {
+      currentView = 'detail';
+      selectedCategory = category;
+    });
+  }
+
+  void backToCategories() {
+    setState(() {
+      currentView = null;
+      selectedCategory = null;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return showAddExpense
-        ? AddExpenseScreen(onBack: backToCategories)
-        : CategoriesPage(onAddPressed: openAddExpense);
+    if (currentView == 'add') {
+      return AddTransictionsScreen(onBack: backToCategories);
+    }
+
+    if (currentView == 'detail' && selectedCategory != null) {
+      return CategoryDetailPage(
+        categoryLabel: selectedCategory!['label'],
+        categoryId: selectedCategory!['id'],
+        iconId: selectedCategory!['iconId'],
+        onAddTransictions: openAddTransictions,
+      );
+    }
+
+    return CategoriesPage(
+      onAddPressed: openAddTransictions,
+      onCategoryPressed: openCategoryDetail,
+    );
   }
 }
